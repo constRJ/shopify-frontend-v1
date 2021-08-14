50dcf8fa4356b3e29f52dc5bbb53bfb4afba0f5a
captmjay <commander.mjay23@gmail.com>
2021-08-14 09:19:25 +0800

    project setup

---
+++ src/utils/processAction.js
@@ -0,0 +1,34 @@
+import request from "@api";
+
+const processAction = (dispatch, option) => {
+  dispatch({
+    type: option.request,
+    payload: { params: option.params, body: option.body },
+  });
+
+  const result = await request[`${option.action}`](
+    `${option.version ? option.version : ""}${option.service}${
+      option.params ? "?" + option.params : ""
+    }`,
+    option.body || "",
+    option.token
+  );
+
+  if (result.error) {
+    dispatch({
+      type: option.failed,
+      payload: result,
+      params: option.params,
+      body: option.body,
+    });
+  } else {
+    dispatch({
+      type: option.success,
+      payload: result,
+    });
+  }
+
+  return result;
+};
+
+export default processAction;
