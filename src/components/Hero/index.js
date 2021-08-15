ef6f11c57fb05e9eea5e3c0b30911f96c46c76c0
captmjay <commander.mjay23@gmail.com>
2021-08-16 06:28:36 +0800

    add waves

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,20 +1,27 @@
+import Lottie from "lottie-web";
 import React, { useEffect, useRef } from "react";
 import { Button, Col, Container } from "react-bootstrap";
-
 const VermaBanner = () => {
-  const animaContainer = useRef()
+  const animaContainer = useRef(null)
 
   useEffect(() => {
-    console.log(animaContainer)
+    Lottie.loadAnimation({
+      container: animaContainer.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      path: "/wave/data-color-1.json"
+    })
   }, [])
   return (
     <Container fluid="lg" className="px-md-5 px-0">
-      <Col className="verma-banner-container d-flex align-items-sm-center align-items-end" ref={animaContainer}>
+      <Col className="verma-banner-container d-flex align-items-sm-center align-items-end">
         <Col xl={3} lg={4} sm={4} xs={8} className="verma-banner-info">
           <h1 className="text-sm-start text-center">Pure All Natural CBD Inspired by the Islands of Hawaii</h1>
           <p className="text-sm-start text-center">ZERO THC - all natural cannabidoil</p>
           <Button className="btn cta-primary">Shop Now</Button>
         </Col>
+        <Col ref={animaContainer} className="verma-banner-wave-1"></Col>
       </Col>
     </Container>
   );
